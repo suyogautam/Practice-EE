9c55caf53e4cd7a8d22841fa59a7fc21eaaededa
suyogautam <suyoggtm@gmail.com>
2025-04-18 10:56:34 -0400

    App.py

--- App.py
+++ App.py
@@ -1,14 +1,15 @@
 import ee
 import streamlit as st
-from pathlib import Path
+import json
 
-# Load token from Streamlit secrets
-ee_token = st.secrets["EARTH_ENGINE_TOKEN"]
+# Load credentials from secrets
+creds = json.loads(st.secrets["EARTH_ENGINE_CREDENTIALS"])
 
-# Write the token to a temporary file (or mimic auth)
-creds_file = Path.home() / ".config/earthengine/credentials"
-creds_file.parent.mkdir(parents=True, exist_ok=True)
-creds_file.write_text(ee_token)
-
-# Initialize GEE
+# Manually initialize (alternative to ee.Initialize())
+ee_token = ee.oauth.Token(
+    creds["refresh_token"],
+    creds["scopes"],
+    creds["redirect_uri"]
+)
+ee.data.setToken(ee_token)
 ee.Initialize()
