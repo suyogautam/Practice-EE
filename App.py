504fec85d25b49d98f9b1678209324b5bfa3fac4
suyogautam <suyoggtm@gmail.com>
2025-04-18 00:31:31 -0400

    Add files via upload

---
+++ App.py
@@ -0,0 +1,34 @@
+Lilly Rush
+Graden Shoemaker
+Will Prokopik
+Collin Stevens, Logan McKay, John Porter, Matthew McGoldrick
+Arwen Downum, Becky Schlesinger, Blake Olsen,
+Hannah Tupaczewski, Savanna Perham, Daniel Woods
+Meg Teasdale, Mary-Kate Easter, Georgia Smith, 
+Jess Boalick, Faith Hemmerdinger, Xavier Burns, Caroline Metheny
+Talia Meeks, Tara Blue, Sithuni Mimasha, Krishna Choudhury
+
+Tim Epperly
+Brenna Demko
+Amanda King
+Erica Liller
+Madeline Trull
+Emma McPhatter, Grace Miller, Reid Pestana, Becky Schlesinger, Maddi William
+Thomas Johnson, Pardis Akbari, Maryam Karami
+Hunter Dunn, Ryan Morel, William Christian,
+Olivia Backer
+Ryan Dillon, Cyrus Wilson, Drew Zacharias, Marwa Al
+
+
+
+Ally Lambiase, Chloe Hurren, Rachel Wilson
+Area conversion to sq. miles is not accurate. I think you might have converted meters to miles instead of sq. meters to sq. miles. (length instead of area)
+
+
+
+
+Ella Aussey, Kieran Sweeney, Max Hartelius, Kieran Precit
+
+Number of pixel is not accurate for area conversion. Its the value of 'histogram' column of attribute table.
+
+Also area conversion to sq. miles is not accurate. 
